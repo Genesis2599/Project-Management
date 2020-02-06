adf809341f9b128537bb30782b2756827fe8d8b2
Genesis2599 <60449144+Genesis2599@users.noreply.github.com>
2020-02-06 13:36:29 -0500

    Netbeans Github work

---
+++ Github Classwork/src/github/classwork/GithubClasswork.java
@@ -0,0 +1,17 @@
+package github.classwork;
+
+//Turrell Martin
+
+import java.util.Scanner;
+
+public class GithubClasswork {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("How is your day");
+        String a = scan.nextLine();
+        System.out.println(a);
+    }
+}
